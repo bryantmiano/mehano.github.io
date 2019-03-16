b96f5457479ea0726478f84d374eb36fc3b86e87
Bryant Miano <bryantmiano@gmail.com>
2019-03-16 11:47:12 -0500

    Updates

---
+++ precache-manifest.20b8a88ddb333a20d887e0b0738ed8b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "46adf5a19b58fafcaf61",
+    "url": "/static/js/main.6690ba28.chunk.js"
+  },
+  {
+    "revision": "b824b81b3680d626844f",
+    "url": "/static/js/2.b7ae4c99.chunk.js"
+  },
+  {
+    "revision": "46adf5a19b58fafcaf61",
+    "url": "/static/css/main.de9e77ec.chunk.css"
+  },
+  {
+    "revision": "45dfc35cca8100b227ace78768785d48",
+    "url": "/index.html"
+  }
+];
--- src/app/redux/pokemon/index.js
+++
@@ -1,4 +0,0 @@
-import pokemonReducer from './reducers';
-export { default as pokemonOperations } from './operations';
-export { default as pokemonTypes } from './types';
-export default pokemonReducer;
